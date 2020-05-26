08d96e46eda3c05096cf504fc1d3c0068d18031e
Maria Politi <politim@uw.edu>
2020-05-25 18:40:24 -0700

    fixed flake8 issues

--- hardy/classifier/Keras_CNN.py
+++ hardy/classifier/Keras_CNN.py
@@ -1,31 +1,53 @@
-import os
 import pickle
-import random
-import shutil
-import sys
 
-import kerastuner as kt
+# import kerastuner as kt
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 
-from keras.layers import (Dense, Dropout, Conv2D, MaxPool2D,
-                          Flatten, MaxPooling2D)
+from keras.layers import (Dense, Conv2D, MaxPool2D,
+                          Flatten)
 from keras.models import Sequential
-from keras.optimizers import Adam, RMSprop
+from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
-from kerastuner.tuners import RandomSearch, BayesianOptimization
-from keras.utils import np_utils
+# from kerastuner.tuners import RandomSearch, BayesianOptimization
 from sklearn.metrics import classification_report, confusion_matrix
-from tensorflow.keras import metrics
 from tensorflow.keras import callbacks
 
 
 def learning_set(path, split=0.1, target_size=(50, 50),
                  classes=['noisy', 'not_noisy'], batch_size=32,
                  color_mode='grayscale', **kwargs):
+    '''
+    A funciton that will create an iterator for the files representing the
+    learning sets
 
+    Parameters
+    ----------
+    path: str
+          A string containing the path to the files to use for the learning set
+    split: float
+            A number between 0 and 1 representing which percentage of the data
+            will compose the validation set
+    target_size: tuple
+                 A tuple containing the dimentions of the image to be inputted
+                 in the model
+    classes: list
+             A list containing strings of the classes the data is divided in.
+             The class name represent the folder name the files are contained
+             in.
+    batch_size: int
+                The number of files to group up into a batch
+    color_mode: str
+                Either grayscale or rgb
+
+    Returns
+    -------
+    training_set:  Keras image iterator
+                The training set containg labelled images
+    validation_set: Keras image iterator
+                The training set containg labelled images
+    '''
     data = ImageDataGenerator(validation_split=split, **kwargs)
     training_set = data.flow_from_directory(path, target_size=target_size,
                                             classes=classes,
@@ -42,10 +64,35 @@ def learning_set(path, split=0.1, target_size=(50, 50),
     return training_set, validation_set
 
 
-def test_set(path, split=0.1, target_size=(50, 50),
+def test_set(path, target_size=(50, 50),
              classes=['noisy', 'not_noisy'], batch_size=32,
              color_mode='grayscale', **kwargs):
+    '''
+    A funciton that will create an iterator for the files representing the
+    test set
 
+    Parameters
+    ----------
+    path: str
+          A string containing the path to the files to use for the test set
+    target_size: tuple
+                 A tuple containing the dimentions of the image to be inputted
+                 in the model
+    classes: list
+             A list containing strings of the classes the data is divided in.
+             The class name represent the folder name the files are contained
+             in.
+    batch_size: int
+                The number of files to group up into a batch
+    color_mode: str
+                Either grayscale or rgb
+
+    Returns
+    -------
+    test_set :  Keras image iterator
+                The testing set containg labelled images that was not part of
+                the learning dataset
+    '''
     data = ImageDataGenerator(**kwargs)
     test_set = data.flow_from_directory(path, target_size=target_size,
                                         classes=classes, batch_size=batch_size,
@@ -62,11 +109,31 @@ def build_model(training_set, validation_set, kernel_size=3, epochs=10,
 
     Parameters
     ----------
+    training_set: Keras image directory iterator
+                  The set of files that will be used to train the CNN model
+    validation_set: Keras image directory iterator
+                    The set of files that will be used to validate the trained
+                    model after each epoch
+    kernel_size: int
+                 Integer indicating teh size of the kernel. The resulting
+                 kernel will be square
+    epochs: int
+            Integer indicating the number of epochs the model will be run for.
+    activation: list
+                A list contianing strings representing the activation function
+                of each layer in the model.
+    input_shape: tuple
+                 A tuple containing the dimentions of the image to be inputted
+                 in the model
 
     Returns
     -------
-
-
+    model: Keras sequential model
+           The trained convolutional neural network
+    history: Keras callbacks function
+                   A function that retains information of the loss and
+                   performance of the training and validation sets in each
+                   epoch.
     '''
     #################################################################
     # Build CNN Model
@@ -99,6 +166,25 @@ def build_model(training_set, validation_set, kernel_size=3, epochs=10,
 
 
 def plot_history(model_history):
+
+    '''
+    Functions that returns plot of the performance of the learning set in each
+    epoch.
+
+    Parameters
+    ----------
+    model_history: Keras callbacks function
+                   A function that retains information of the loss and
+                   performance of the training and validation sets in each
+                   epoch.
+
+    Returns
+    -------
+    fig: matplotlib plot
+         A figure containing two plots showing the change in the loss and
+         accuracy during the training of the model
+
+    '''
     # Let's plot the results
     fig, ax = plt.subplots(1, 2, figsize=(8, 6))
     plt.subplots_adjust(wspace=0.5)
@@ -125,7 +211,7 @@ def plot_history(model_history):
     ax[1].legend()
     ax[1].set_title('CNN Accuracy per Epoch')
 
-    return
+    return plt.show()
 
 
 def evaluate_model(model, testing_set):
@@ -136,9 +222,9 @@ def evaluate_model(model, testing_set):
 
     Parameters
     ----------
-    model :  keras model ####
+    model :  keras sequential model
             the trained model we want to evaluate using a testing set
-    testing_set: ######
+    testing_set: Keras image directory iterator
                 The testing set containg labelled images that was not part of
                 the learning dataset. This will be used to evaluate the actual
                 performance of the trained model.
@@ -159,7 +245,26 @@ def evaluate_model(model, testing_set):
 
 def report_on_metrics(model, test_set, target_names=['noisy', 'not_noisy']):
     '''
+    A function that prints the result of the model just trained
 
+    Parameters
+    ----------
+    model: Keras sequential model
+           the trained convolutional neural network
+    test_set: Keras image directory iterator
+              the test set to use to obtain the true performance of the model.
+    target_names: list
+                  list containing strings represnting the classes the data
+                  is classified in
+
+    Returns
+    -------
+    conf_matrix : array
+                  A numpy array containing values for the true positives,
+                  false negatives, false positives and true negatives
+    report : str
+             a string containg the overall report of the performance
+             of the model. Accuracy, recall and F1 scores are reported.
     '''
     Y_pred = model.predict_generator(test_set, len(test_set))
     y_pred = np.argmax(Y_pred, axis=1)
@@ -197,36 +302,27 @@ def save_load_model(filename, model=None, save=None, load=None):
     '''
     if save:
         pickle.dump(model, open(filename+'.sav', 'wb'))
-        return 0
+        return 'the model was correctly saved'
     elif load:
         loaded_model = pickle.load(open(filename+'.sav', 'rb'))
         return loaded_model
 
-#
-# def cross_validation(k, path, split=0.2, target_size=(50, 50),
-#                      classes=['noisy', 'not_noisy'], batch_size=32,
-#                      color_mode='rgb', kernel_size=3, epochs=5):
-#     training_set, validation_set = learning_set(path, split=split,
-#                                                 target_size=target_size,
-#                                               classes=['noisy', 'not_noisy'],
-#                                                 batch_size=batch_size,
-#                                                 color_mode='rgb'):
-#
-#
-#     return
-
 
 def build_tuner_model(hp):
-    '''Builds a convolutional keras model with tunable hyperparameters
+    '''
+    Functions that builds a convolutional keras model with
+    tunable hyperparameters
 
 
     Parameters
     ----------
-
+    hp: keras tuner class
+        A class that is used to define the parameter search space
 
     Returns
     -------
-
+    model: Keras sequential model
+           The trained convolutional neural network
     '''
     inputs = tf.keras.Input(shape=(50, 50, 3))
     x = inputs
@@ -237,16 +333,19 @@ def build_tuner_model(hp):
             activation=hp.Choice('activation_' + str(i),
                                  ['relu', 'sigmoid']))(x)
 
-#     if hp.Choice('global_pooling', ['max', 'avg']) == 'max':
     x = tf.keras.layers.GlobalMaxPooling2D()(x)
-#     else:
-#         x = tf.keras.layers.GlobalAveragePooling2D()(x)
+
     outputs = tf.keras.layers.Dense(2, activation='softmax')(x)
 
     model = tf.keras.Model(inputs, outputs)
 
     optimizer = hp.Choice('optimizer', ['adam', 'sgd'])
-    learning_Rate = hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4])
+    learning_rate = hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4])
     model.compile(optimizer, leraning_rate=learning_rate,
                   loss='categorical_crossentropy', metrics=['accuracy'])
     return model
+
+
+# def run_tuner():
+#
+#     return
