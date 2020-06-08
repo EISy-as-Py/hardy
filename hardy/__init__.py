079da6a7c7460a4dccd43dbfaf31652862c4cb94
Maria Politi <politim@uw.edu>
2020-06-08 11:03:49 -0700

    modified __init__.py file to matche new wrapper module name

--- hardy/__init__.py
+++ hardy/__init__.py
@@ -3,7 +3,7 @@ from .arbitrage import *                 # noqa: F401, F403
 from .recognition import *               # noqa: F401, F403
 from .handling import *                  # noqa: F401, F403
 from .ynot import *                      # noqa: F401, F403
-from .hardy import *                     # noqa: F401, F403
+from .run_hardy import *                 # noqa: F401, F403
 # __all__ = [""]
 #
 name = 'hardy'
