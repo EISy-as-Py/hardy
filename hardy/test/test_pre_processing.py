543c9f12d5b99b2b728efcf3f2b27cc61bbabb93
Maria Politi <politim@uw.edu>
2020-06-09 14:59:26 -0700

    finished test functions for pre_processing module, removed old jup. notebooks

--- hardy/test/test_pre_processing.py
+++ hardy/test/test_pre_processing.py
@@ -1,15 +1,15 @@
-# import keras
+import os
+import shutil
 import unittest
 
 import numpy as np
-import pandas as pd
+# import pandas as pd
 
-from hardy.handling.to_catalogue import learning_set, test_set
+# from hardy.handling.to_catalogue import learning_set, test_set
 from hardy.handling import to_catalogue as catalogue
 from hardy.handling import pre_processing as preprocessing
 
-path = './hardy/test/test_image/'
-data_path = './hardy/test/test_data/'
+data_path = './hardy/test/test_folder_split/'
 
 
 class TestSimulationTools(unittest.TestCase):
@@ -38,11 +38,36 @@ class TestSimulationTools(unittest.TestCase):
         pass
 
     def test_set_folder(self):
-        
+        num_files = 5
+        # Frm .csv files
+        test_set_filenames = preprocessing.hold_out_test_set(
+            data_path, number_of_files_per_class=num_files)
+        test_folder = preprocessing.test_set_folder(
+            data_path, test_set_filenames)
+        assert isinstance(test_folder, str), \
+            'the return should be the path to the test folder'
+        assert os.path.exists(test_folder), \
+            'the test folder was not correctly created'
         pass
 
     def test_classes_folder_split(self):
+        list_of_folders = preprocessing.classes_folder_split(
+            data_path, classes=['noise', ''],
+            class_folder=['noisy', 'not_noisy'], file_extension='.csv')
+        assert isinstance(list_of_folders, list), \
+            'the output should be a list'
+        for path in list_of_folders:
+            assert os.path.exists(path), \
+                'the class fodler was not correctly created'
+            # move the files out of the folder for ensuring
+            # the next test iteration works
+            for file in [n for n in os.listdir(path)]:
+                shutil.move(path + file, data_path)
         pass
 
     def test_save_to_folder(self):
+        folder_path = preprocessing.save_to_folder(
+            data_path, 'test_project', 'test_1')
+        assert os.path.exists(folder_path), \
+            'the fodler was not correctly created'
         pass
