983656d6f1f0b346049f071f3deb052da549b7c2
amoeezuw <amoeez@uw.edu>
2020-06-15 10:15:25 -0700

    checking travis with change in input name

--- hardy/recognition/tuner.py
+++ hardy/recognition/tuner.py
@@ -38,9 +38,9 @@ def build_tuner_model(hp):
     ####################################
     # Defining input size
     # need to put input shape in the config file
-    input = (param['input_shape'][0], param['input_shape'][0],
-             param['input_shape'][1])
-    inputs = tf.keras.Input(shape=input)
+    input_x = (param['input_shape'][0], param['input_shape'][0],
+               param['input_shape'][1])
+    inputs = tf.keras.Input(shape=input_x)
     x = inputs
 
     ####################################
