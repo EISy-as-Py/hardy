81da4442347f722a09df20dc4eae89264b5e7f89
Maria Politi <politim@uw.edu>
2020-06-15 20:50:49 -0700

    added new module for data reporting after overall run

---
+++ hardy/data_reporting/__init__.py
@@ -0,0 +1,10 @@
+from .version import __version__      # noqa: F401
+from .data_reporting import *         # noqa: F401, F403
+from .handling import *               # noqa: F401, F403
+from .to_catalogue import *           # noqa: F401, F403
+from .visualization import *          # noqa: F401, F403
+from .pre_processing import *         # noqa: F401, F403
+
+# __all__ = [__version__]
+
+name = 'reporting'
---
+++ hardy/data_reporting/reporting.py
@@ -0,0 +1,7 @@
+# add funcitons to summarize and visualize the summary reports from the
+# the hardy run
+
+
+# def report_plot():
+# def reports_summary:
+# def performance_comparison():
