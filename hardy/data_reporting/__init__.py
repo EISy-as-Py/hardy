5a7c58a6d25172ae938206854facdb4fc7d5c877
amoeezuw <amoeez@uw.edu>
2020-06-15 21:57:03 -0700

    fixing the init files for data_reporting package

--- hardy/data_reporting/__init__.py
+++ hardy/data_reporting/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__      # noqa: F401
-from .data_reporting import *         # noqa: F401, F403
+from .reporting import *         # noqa: F401, F403
 from .handling import *               # noqa: F401, F403
 from .to_catalogue import *           # noqa: F401, F403
 from .visualization import *          # noqa: F401, F403
@@ -7,4 +7,4 @@ from .pre_processing import *         # noqa: F401, F403
 
 # __all__ = [__version__]
 
-name = 'reporting'
+name = 'data_reporting'
