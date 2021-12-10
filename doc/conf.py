a01b08e2f3ba879edb5f222f26823de8b87729f6
amoeezuw <amoeez@uw.edu>
2021-12-10 10:55:58 +0500

    chainging the theme to reku to test plotly

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'renku'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
